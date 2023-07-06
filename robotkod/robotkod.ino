0f1c2106420f8cc9b5b19ef6e59bbd3ffefac526
teknoway <137414501+teknoway@users.noreply.github.com>
2023-07-06 16:58:26 +0300

    Update robotkod.ino

--- robotkod/robotkod.ino
+++ robotkod/robotkod.ino
@@ -1,65 +1,57 @@
+
 #include "deneyap.h"
+#include "NewPing.h" 
 #include <Deneyap_Servo.h>
-#include "NewPing.h"
-#define TRIGGER_PIN D1
-#define ECHO_PIN D0
-#define MAX_DISTANCE 400  
-#define srv1 D12
-#define srv2 D13
-#define srv3 D14
 #define bzr D9
+#define led D4
+#define SOUND_SPEED 0.034
+#define CM_TO_INCH 0.393701
+#define trigPin 14
+#define echoPin 13
+long duration;
+float distanceCm;
+float distanceInch;
 Servo servo1;
 Servo servo2;
-Servo servo3;
-NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE);
-float duration, distance;
-void mesafeolc()
-{
 
-  distance = sonar.ping_cm();
-  
-  Serial.print("Mesafe = ");
-if(distance<10){
-  tone(bzr,155);
-  delay(50);
-  noTone(bzr);
-  servo1.write(-180);
-  
-  delay(100);
-}
-else{
-    servo1.write(-180);
+
+void setup() {
+  servo1.attach(D0);
+  servo2.attach(D1);
+  pinMode(bzr,OUTPUT);
+  pinMode(led,OUTPUT);
+   pinMode(D14,OUTPUT);
+  pinMode(D13,INPUT);
+    pinMode(D8,INPUT);
 
 }
-  delay(500);
+void mesafe_olc() {
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  duration = pulseIn(echoPin, HIGH);
+  distanceCm = duration * SOUND_SPEED/2;
+  distanceInch = distanceCm * CM_TO_INCH;
+  Serial.print("Distance (cm): ");
+  Serial.println(distanceCm);
+  Serial.print("Distance (inch): ");
+  Serial.println(distanceInch);
+  delay(1000);
 }
-void ileri()
-{
+void ileri(){
   int sw1=digitalRead(D8);
   Serial.println(sw1);
-  if(sw1==0){
-  servo1.write(360);
-  delay(100);
-  servo2.write(360);
+  if(sw1==0||distanceCm<10)
+  {
+  servo1.write(90);
+  delay(500);
+  servo2.write(90);
   }
-delay(100);
+}
+void loop() {
+mesafe_olc(); 
+ileri();
 
-}
- 
-void setup() 
-{
-  Serial.begin(9600);
-  servo1.attach(srv1);
-  servo2.attach(srv2);
-  servo3.attach(srv3);
-  servo1.write(0);
-  servo2.write(180);
-  servo3.write(180);
-  pinMode(D4,INPUT);
-   pinMode(D8,INPUT);
-}
-void loop() 
-{
-  mesafeolc();
-  ileri();
 }
